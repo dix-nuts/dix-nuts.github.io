16b4e4e2aa0362862c023e773b393dea14381000
dix-nuts <94485100+dix-nuts@users.noreply.github.com>
2022-01-30 21:41:31 -0500

    Update accesswebsite.js

--- accesswebsite.js
+++ accesswebsite.js
@@ -8,9 +8,9 @@ function myFunction() {
   let person = prompt("Please enter the password:", "");
   if (person == null || person == "") {
     text = "Too scared to guess the password?";
-  } else if (person == "iris is so hot and tall and smart") {
-  window.location = 'https://eyeris-who.github.io/information.html';
-   text = "I know I am :)";
+  } else if (person == "ducks are tasty") {
+  window.location = 'https://dix-nuts.github.io/information.html';
+   text = "Tru";
   } else {
     text = "Get rekt lmao.";
     openWin();
