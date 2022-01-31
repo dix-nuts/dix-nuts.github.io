3a31c28ede0f7396a4cb8f94723e84c97efcd402
dix-nuts <94485100+dix-nuts@users.noreply.github.com>
2022-01-30 22:02:22 -0500

    Update accesswebsite.js

--- accesswebsite.js
+++ accesswebsite.js
@@ -1,5 +1,6 @@
 function openWin() {
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  myWindow = window.open("rickroll.html", "_self");
+  setTimeout(myWindow.close(), 3000);
 }
 
 function myFunction() {
@@ -7,9 +8,9 @@ function myFunction() {
   let person = prompt("Please enter the password:", "");
   if (person == null || person == "") {
     text = "Too scared to guess the password?";
-  } else if (person == "ducks are tasty") {
-  window.location = 'https://dix-nuts.github.io/information.html';
-   text = "Tru";
+  } else if (person == "iris is so hot and tall and smart") {
+  window.location = 'https://eyeris-who.github.io/information.html';
+   text = "I know I am :)";
   } else {
     text = "Get rekt lmao.";
     openWin();
