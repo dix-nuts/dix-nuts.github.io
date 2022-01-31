10095950b400db0f1bdb1ba0b03c3d1774d56108
dix-nuts <94485100+dix-nuts@users.noreply.github.com>
2022-01-30 22:25:21 -0500

    Update accesswebsite.js

--- accesswebsite.js
+++ accesswebsite.js
@@ -1,5 +1,6 @@
 function openWin() {
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  myWindow = window.open("rickroll.html", "_self");
+  setTimeout(myWindow.close(), 3000);
 }
 
 function myFunction() {
