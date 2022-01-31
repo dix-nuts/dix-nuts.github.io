75866dfc08a1f42d188034a215fec3eebd5ec050
dix-nuts <94485100+dix-nuts@users.noreply.github.com>
2022-01-30 21:34:21 -0500

    Update accesswebsite.js

--- accesswebsite.js
+++ accesswebsite.js
@@ -1,13 +1,19 @@
+function openWin() {
+  myWindow = window.open("rickroll.html", "_self");
+  setTimeout(myWindow.close(), 3000);
+}
+
 function myFunction() {
   let text;
   let person = prompt("Please enter the password:", "");
   if (person == null || person == "") {
     text = "Too scared to guess the password?";
-  } else if (person == "ducks are tasty") {
-  window.location = 'https://dix-nuts.github.io/information.html';
-   text = "Duck Ha is tastiest";
+  } else if (person == "iris is so hot and tall and smart") {
+  window.location = 'https://eyeris-who.github.io/information.html';
+   text = "I know I am :)";
   } else {
     text = "Get rekt lmao.";
+    openWin();
   }
   document.getElementById("demo").innerHTML = text;
 }
